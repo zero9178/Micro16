9003e7e0e0b7100a5d915dc6ba526cb47bd19d8b
zero9178 <markus.boeck02@gmail.com>
2021-05-14 14:36:57 +0200

    Added micro sequenzing logic and testbench

---
+++ micro_seq.vhd
@@ -0,0 +1,20 @@
+
+use work.isa.all;
+
+entity micro_seq is
+	port(condition : in cond_op;
+		  negative_flag,zero_flag : in bit;
+		  do_jump : out bit);
+end entity;
+
+architecture behavioural of micro_seq is
+
+begin
+
+	with condition select
+		do_jump <= '0' when cond_no_jump,
+					  negative_flag when cond_neg,
+					  zero_flag when cond_zero,
+					  '1' when cond_none;
+
+end architecture;
